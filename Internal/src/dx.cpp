36980d320d7a721b8e4cdf6c398dca20c9d977cc
wilszdev <wilsonzak932@gmail.com>
2021-12-06 15:26:38 +1100

    reorganised code for d3d hooks

--- Internal/src/dx.cpp
+++ Internal/src/dx.cpp
@@ -10,40 +10,25 @@ extern void hkReset();
 
 namespace dx
 {
-	#define RESET_PATCH_BYTECOUNT 5
+#define RESET_PATCH_BYTECOUNT 5
+#define END_SCENE_PATCH_BYTECOUNT 7
 
 	void* deviceVtable[119] = { 0 };
-	uint8_t EndSceneOriginalBytes[7] = { 0 };
+	uint8_t EndSceneOriginalBytes[END_SCENE_PATCH_BYTECOUNT] = { 0 };
 	EndScene_fn* originalEndScene = nullptr;
 	uint8_t ResetOriginalBytes[RESET_PATCH_BYTECOUNT] = { 0 };
 	Reset_fn* originalReset = nullptr;
-
-	RESET(hkReset)
-	{
-		// release our resources here
-		::hkReset();
-		return originalReset(device, params);
-	}
-
-
-	void CreateEndSceneHookLoopUntilSuccess(HWND window)
-	{
-		while (!CopyVtable(window)) { Sleep(1000); }
-		// EndScene is the 43rd entry in the vftable (index 42)
-		memcpy(EndSceneOriginalBytes, deviceVtable[42], 7);
-		originalEndScene = (EndScene_fn*)TrampolineHook(deviceVtable[42], dx::hkEndScene, 7);
-	}
 
 	bool HookDirect3D(HWND window)
 	{
 		if (!CopyVtable(window))
 			return false;
 
-		printf("Locating D3D methods...\n EndScene is at %p\n Reset is at %p\n DrawIndexedPrimitive is at %p\n", 
+		printf("Locating D3D methods...\n EndScene is at %p\n Reset is at %p\n DrawIndexedPrimitive is at %p\n",
 			deviceVtable[42], deviceVtable[16], deviceVtable[82]);
 		// EndScene is the 43rd entry in the vftable (index 42)
-		memcpy(EndSceneOriginalBytes, deviceVtable[42], 7);
-		originalEndScene = (EndScene_fn*)TrampolineHook(deviceVtable[42], dx::hkEndScene, 7);
+		memcpy(EndSceneOriginalBytes, deviceVtable[42], END_SCENE_PATCH_BYTECOUNT);
+		originalEndScene = (EndScene_fn*)TrampolineHook(deviceVtable[42], dx::hkEndScene, END_SCENE_PATCH_BYTECOUNT);
 
 		memcpy(ResetOriginalBytes, deviceVtable[16], RESET_PATCH_BYTECOUNT);
 		originalReset = (Reset_fn*)TrampolineHook(deviceVtable[16], dx::hkReset, RESET_PATCH_BYTECOUNT);
@@ -52,8 +37,8 @@ namespace dx
 
 	void UnhookDirect3D()
 	{
-		puts("unhooking");
-		Patch(deviceVtable[42], EndSceneOriginalBytes, 7);
+		puts("unhooking d3d");
+		Patch(deviceVtable[42], EndSceneOriginalBytes, END_SCENE_PATCH_BYTECOUNT);
 		Patch(deviceVtable[16], ResetOriginalBytes, RESET_PATCH_BYTECOUNT);
 		puts(" done");
 	}
@@ -102,5 +87,12 @@ namespace dx
 	{
 		::hkEndScene(device);
 		return originalEndScene(device);
+	}
+
+	RESET(hkReset)
+	{
+		// release our resources here
+		::hkReset();
+		return originalReset(device, params);
 	}
 }
