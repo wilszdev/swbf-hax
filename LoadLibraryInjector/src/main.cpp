4ed65db281e32dcc1d2423734999335dd5b9e809
wilszdev <wilsonzak932@gmail.com>
2021-12-06 17:04:06 +1100

    added relative path expansion to injector

--- LoadLibraryInjector/src/main.cpp
+++ LoadLibraryInjector/src/main.cpp
@@ -25,12 +25,31 @@ int main(int argc, char** argv)
 	if (!memory) return -1;
 	printf("allocated memory at 0x%04zx\n", (uintptr_t)memory);
 
-	if (!WriteProcessMemory(process, memory, argv[2], strlen(argv[2]) + 1, 0))
+	DWORD length = GetFullPathNameA(argv[2], 0, 0, 0);
+	char* buffer = new char[length];
+
+	if (!GetFullPathNameA(argv[2], length, buffer, 0))
 	{
+		delete[] buffer;
+		puts("[-] failed to expand relative path");
+		return -1;
+	}
+
+	if (GetFileAttributesA(buffer) == INVALID_FILE_ATTRIBUTES)
+	{
+		delete[] buffer;
+		printf("[-] could not find file \"%s\"\n", argv[2]);
+		return -1;
+	}
+
+	if (!WriteProcessMemory(process, memory, buffer, length, 0))
+	{
+		delete[] buffer;
 		VirtualFreeEx(process, memory, 0, MEM_RELEASE);
 		return -1;
 	}
-	printf("wrote dll path ('%s') into memory\n", argv[2]);
+	printf("wrote dll path ('%s') into memory\n", buffer);
+	delete[] buffer;
 
 	HANDLE thread = CreateRemoteThread(process, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, memory, 0, 0);
 	if (!thread || thread == INVALID_HANDLE_VALUE)
@@ -39,7 +58,7 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	printf("created remote thread, handle 0x%04zx\n", (uintptr_t)thread);
-	
+
 	WaitForSingleObject(thread, INFINITE);
 	CloseHandle(thread);
 
