7ed712ec8f2a4b18cc421b4a28e7e249e812bead
wilszdev <wilsonzak932@gmail.com>
2021-12-29 10:40:21 +1000

    hook.cpp - remove debug output

--- Internal/src/hook.cpp
+++ Internal/src/hook.cpp
@@ -1,7 +1,6 @@
 #include "WindowsLessBloat.h"
 #include "hook.h"
 #include <cstdint>
-#include <cstdio>
 
 void Patch(void* dst, void* src, size_t length)
 {
@@ -40,14 +39,10 @@ void* TrampolineHook(void* src, void* dst, size_t length)
 
 	if (*(char*)src == (char)0xE9)
 	{
-		printf("the was a jmp instruction at the hook destination (%p)\n", src);
 		intptr_t jmpOffset = 0;
 		memcpy(&jmpOffset, (char*)src + 1, sizeof(intptr_t));
-		printf(" the jmp has a rel offset of 0x%zx\n", jmpOffset);
 		intptr_t jmpDst = (intptr_t)src + jmpOffset + 5;
-		printf(" the jmp destination is 0x%zx\n", jmpDst);
 		intptr_t newOffset = ((intptr_t)jmpDst) - ((intptr_t)gateway + 5);
-		printf(" new rel offset: 0x%zx\n", newOffset);
 		memcpy((char*)gateway + 1, &newOffset, sizeof(intptr_t));
 	}
 
