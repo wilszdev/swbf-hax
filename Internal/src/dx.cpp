1315f14984e214e4665426bcb5ae30868bc640b2
wilszdev <wilsonzak932@gmail.com>
2021-12-29 10:31:31 +1000

    dx.cpp - remove debug prints to console

--- Internal/src/dx.cpp
+++ Internal/src/dx.cpp
@@ -1,6 +1,5 @@
 #include "dx.h"
 #include "hook.h"
-#include <cstdio>
 
 extern void hkDirectX_EndScene(LPDIRECT3DDEVICE9 device);
 extern void hkDirectX_Reset();
@@ -24,23 +23,21 @@ namespace dx
 		if (!CopyVtable(window))
 			return false;
 
-		printf("Locating D3D methods...\n EndScene is at %p\n Reset is at %p\n",
-			deviceVtable[42], deviceVtable[16]);
 		// EndScene is the 43rd entry in the vftable (index 42)
 		memcpy(EndSceneOriginalBytes, deviceVtable[42], END_SCENE_PATCH_BYTECOUNT);
 		originalEndScene = (EndScene_fn*)TrampolineHook(deviceVtable[42], dx::hkDirectX_EndScene, END_SCENE_PATCH_BYTECOUNT);
 
+		// Reset is the 17th entry in the vftable
 		memcpy(ResetOriginalBytes, deviceVtable[16], RESET_PATCH_BYTECOUNT);
 		originalReset = (Reset_fn*)TrampolineHook(deviceVtable[16], dx::hkDirectX_Reset, RESET_PATCH_BYTECOUNT);
+
 		return true;
 	}
 
 	void RemoveHooks()
 	{
-		puts("unhooking d3d");
 		Patch(deviceVtable[42], EndSceneOriginalBytes, END_SCENE_PATCH_BYTECOUNT);
 		Patch(deviceVtable[16], ResetOriginalBytes, RESET_PATCH_BYTECOUNT);
-		puts(" done");
 	}
 
 	bool CopyVtable(HWND window)
